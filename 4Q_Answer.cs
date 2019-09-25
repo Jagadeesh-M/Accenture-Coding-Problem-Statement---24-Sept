84a15519881987aae5cf6d1c4b9e0b04cc2f086a
Jagadeesh <jagadeesh0813@gmail.com>
2019-09-25 08:00:43 +0530

    Add files via upload

---
+++ 4Q_Answer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "Reverse The Words In A String";
+            string reversedString_1 = ReverseWordsInString_1(inputString);
+            /*+++ This is reversing words of a string using Split and Reverse +++*/
+                Console.Write(reversedString_1);
+            Console.WriteLine("\n---------------------------------------------------------------------------------------------\n");
+            string reversedString_2 = ReverseWordsInString_2(inputString);
+            /*+++ This is reversing words of a string WITHOUT using Split and Reverse +++*/
+                Console.WriteLine(reversedString_2);
+        }
+        
+        /*+++ This is reversing words of a string using Split and Reverse +++*/
+        public static string ReverseWordsInString_1(string inputString)
+        {
+            string[] str = inputString.Split(' ');
+            StringBuilder reversedString = new StringBuilder();
+            Array.Reverse(str);
+            foreach(string s in str)
+            {
+                reversedString.Append(s.ToString() + " ");
+            }
+            return reversedString.ToString();
+        }
+        /*--- This is reversing words of a string using Split and Reverse ---*/
+
+        /*+++ This is reversing words of a string WITHOUT using Split and Reverse +++*/
+        public static string ReverseWordsInString_2(string inputString)
+        {
+            ArrayList inputStringArrayList = new ArrayList();
+            string splittedWords = "";
+            StringBuilder reversedString = new StringBuilder();
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] != ' ')
+                {
+                    splittedWords = splittedWords + inputString[i];
+                    if (i == (inputString.Length - 1))
+                        inputStringArrayList.Add(splittedWords);
+                    continue;
+                }
+                inputStringArrayList.Add(splittedWords);
+                splittedWords = "";
+            }
+            for (int i = inputStringArrayList.Count; i >= 1; i--)
+            {
+                reversedString.Append(inputStringArrayList[i - 1] + " ");
+            }
+            return reversedString.ToString();
+        }
+        /*--- This is reversing words of a string WITHOUT using Split and Reverse ---*/
+    }
+}
---
+++ 5Q_Answer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentenceWithDuplicates = new string[5] { "Simple","Code","and","Clean","Code"};
+            var output = RemoveDuplicateWords(sentenceWithDuplicates);
+            for (int j = 0; j < output.Length; j++)
+                Console.WriteLine(output[j]);
+        }
+        public static string[] RemoveDuplicateWords(string[] sentenceWithDuplicateWords)
+        {
+            var sentenceWithOutDuplicates = new ArrayList();
+
+            for (int i = 0; i < sentenceWithDuplicateWords.Length; i++)
+            {
+                if (!sentenceWithOutDuplicates.Contains(sentenceWithDuplicateWords[i]))
+                    sentenceWithOutDuplicates.Add(sentenceWithDuplicateWords[i]);
+            }
+            return (string[])sentenceWithOutDuplicates.ToArray(typeof(string));
+        }
+    }
+}
